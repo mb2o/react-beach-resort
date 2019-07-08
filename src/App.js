6de7ac8ca7850d2e08bde83451ea3eb965314f72
mb2o <mboomaars@gmail.com>
2019-07-08 21:16:30 +0200

    Page Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import Single from './pages/Single';
 import './App.css';
 
 function App() {
   return (
     <Fragment>
-      <h2>App Component</h2>
+      <Router>
+        <Switch></Switch>
+      </Router>
+      <Home />
+      <Rooms />
+      <Single />
+      <Error />
     </Fragment>
   );
 }
---
+++ src/pages/Single.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Single extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Singe Room Page</h2>
+      </div>
+    );
+  }
+}
