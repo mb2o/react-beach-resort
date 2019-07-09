006d09345856516231215324b992bcfaec756123
mb2o <mboomaars@gmail.com>
2019-07-09 11:18:17 +0200

    Banner Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Banner from '../components/Banner';
 import Hero from '../components/Hero';
 
 const Home = () => {
   return (
     <Hero>
-      <button />
+      <Banner title="Luxurious rooms" subtitle="Deluxe rooms starting at $299">
+        <Link to="/rooms" className="btn btn-primary">
+          Our rooms
+        </Link>
+      </Banner>
     </Hero>
   );
 };
