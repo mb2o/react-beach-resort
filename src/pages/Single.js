79a117fb1bf055a096f1f1d1fcde42eb82ee80b2
mb2o <mboomaars@gmail.com>
2019-07-09 20:53:41 +0200

    Single Room Complete

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -1,9 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import defaultBcg from '../images/room-1.jpeg';
-import Hero from '../components/Hero';
-import Banner from '../components/Banner';
 import { RoomContext } from '../context';
+import Banner from '../components/Banner';
+import defaultBcg from '../images/room-1.jpeg';
 import StyledHero from '../components/StyledHero';
 
 export default class Single extends Component {
@@ -45,15 +44,49 @@ export default class Single extends Component {
       images
     } = room;
 
+    const [mainImg, ...roomImgs] = images;
+
     return (
       <Fragment>
-        <StyledHero img={images[0]}>
+        <StyledHero img={mainImg}>
           <Banner title={`${name} room`}>
             <Link to="/rooms" className="btn-primary">
               Back to rooms
             </Link>
           </Banner>
         </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {roomImgs.map((image, index) => (
+              <img key={index} src={image} alt={room.name} />
+            ))}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price: ${price}</h6>
+              <h6>size: {size} SQFT</h6>
+              <h6>
+                max capacity:{' '}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+              <h6>{breakfast && 'free breakfast included'}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((extra, index) => (
+              <li key={index}>- {extra}</li>
+            ))}
+          </ul>
+        </section>
       </Fragment>
     );
   }
