393f1fe78624b0af5e929d939cb848d04755d206
mb2o <mboomaars@gmail.com>
2019-07-09 20:12:26 +0200

    Single room setup

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { RoomContext } from '../context';
 
 export default class Single extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg
+    };
+  }
+
+  static contextType = RoomContext;
+
   render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    console.log(room);
+
     return (
       <div>
         <h2>Singe Room Page</h2>
